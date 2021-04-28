0584ebbf43084b9ef6b8c2af13412f43b29f217c
Matheus <matheuspsilva29@gmail.com>
2021-04-27 22:58:08 -0300

    products endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function (Request $request) {
+
+    $response = new \Illuminate\Http\Response(json_encode(['msg' => 'Minha Primeira resposta de API']));
+    $response->header('Content-Type', 'application/json');
+
+    return $response;
+});
+
+//Products Route
+Route::get('products', [ProductController::class, 'index']);
